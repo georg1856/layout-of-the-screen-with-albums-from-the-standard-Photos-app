a20b21d894bb933870d604aba730be6c5a249c4a
Georgiy Polukhin <lev.davidovich1@yandex.ru>
2022-06-18 12:49:18 +0300

    Create CellModel

--- layout-of-the-screen-with-albums-from-the-standard-Photos-app/layout-of-the-screen-with-albums-from-the-standard-Photos-app/PhotoGalleryViewController.swift
+++ layout-of-the-screen-with-albums-from-the-standard-Photos-app/layout-of-the-screen-with-albums-from-the-standard-Photos-app/PhotoGalleryViewController.swift
@@ -7,23 +7,16 @@
 
 import UIKit
 
+struct CellModel: Hashable {
+    let imageName: String
+    let labelName: String
+    let labelCount: String
+}
+
 class PhotoGalleryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
